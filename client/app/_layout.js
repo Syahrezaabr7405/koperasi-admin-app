5fe4b7d308d939473403d34957e3aca116022778
Syahrezaabr7405 <rezaax7405@gmail.com>
2026-04-02 12:46:17 +0700

    add favicon logo

--- client/app/_layout.js
+++ client/app/_layout.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Stack } from 'expo-router';
 import { CartProvider } from '../src/CartContext';
-import { StatusBar, SafeAreaView, View } from 'react-native'; 
+import { StatusBar, SafeAreaView } from 'react-native'; 
+import Head from 'expo-router/head'; // 1. Tambahkan import ini
 
 export default function RootLayout() {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      {/* 2. Tambahkan Head untuk konfigurasi Web */}
+      <Head>
+        <title>Koperasi App</title>
+        <meta name="description" content="Aplikasi Koperasi JKNH" />
+        <link rel="icon" href="/assets/favicon.png" /> 
+      </Head>
+
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
       
       <CartProvider>
